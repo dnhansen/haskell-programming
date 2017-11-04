926fd0163fb71657c2bc695f984c591677dbe15f
Danny Nygård Hansen <dannynhansen@gmail.com>
2017-11-04 14:26:41 +0100

    First commit. Completed most of the chapter 2 exercises (except 2.13), but only wrote some of them down here.

---
+++ ch02.hs
@@ -0,0 +1,24 @@
+-- 2.2 Interacting with Haskell code
+
+sayHello :: String -> IO ()
+sayHello x = putStrLn ("Hello, " ++ x ++ "!")
+
+
+-- 2.4 Functions
+
+triple x = x * 3
+
+
+-- -- Intermission: Exercises
+
+circleArea r = 3.14 * r * r
+
+
+-- 2.5 Infix operators
+
+-- -- Intermission: Exercises
+
+
+-- 2.6 Declaring values
+
+-- See 'learn.hs'
---
+++ learn.hs
@@ -0,0 +1,10 @@
+-- learn.hs
+-- 2.6 Declaring values
+
+module Learn where
+
+x = 10 * 5 + y
+
+myResult = x * 5
+
+y = 10
